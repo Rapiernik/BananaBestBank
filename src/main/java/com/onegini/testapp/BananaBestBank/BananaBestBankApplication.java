831643565609db2a542a92a9a6119ef613b54164
Rafal <rapiernik@gmail.com>
2018-05-24 21:57:29 +0200

    init

---
+++ src/main/java/com/onegini/testapp/BananaBestBank/BananaBestBankApplication.java
@@ -0,0 +1,12 @@
+package com.onegini.testapp.BananaBestBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BananaBestBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BananaBestBankApplication.class, args);
+	}
+}
